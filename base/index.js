3c12ed72cf692421004f77c0ba0f8410a81b4df1
DHN Chandan <php.chandan@gmail.com>
2023-03-25 00:24:12 +0600

    refactor: :wrench: Update base eslint config

--- base/index.js
+++ base/index.js
@@ -1,76 +1,77 @@
 module.exports = {
-	extends: [
-		"eslint:recommended",
-		"plugin:import/typescript",
-		"plugin:prettier/recommended",
-	],
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: ["./tsconfig.json"],
-		ecmaVersion: "latest",
-		ecmaFeatures: {
-			jsx: true,
-		},
-		sourceType: "module",
-	},
-	env: {
-		es6: true,
-		es2020: true,
-		es2021: true,
-		commonjs: true,
-		node: true,
-		browser: true,
-		worker: true,
-		jest: true,
-		"shared-node-browser": true,
-	},
-	globals: {
-		Atomics: "readonly",
-		SharedArrayBuffer: "readonly",
-	},
-	settings: {
-		"import/resolver": {
-			typescript: {
-				alwaysTryTypes: true,
-			},
-		},
-		"import/parsers": {
-			"@typescript-eslint/parser": [".ts", ".tsx"],
-		},
-	},
-	plugins: ["@typescript-eslint", "prettier", "import"],
-	rules: {
-		"@typescript-eslint/naming-convention": [
-			"error",
-			{
-				selector: "variable",
-				types: ["boolean"],
-				format: ["PascalCase"],
-				prefix: ["is", "should", "has", "can", "did", "will"],
-			},
-			{
-				selector: "function",
-				format: ["PascalCase", "camelCase"],
-			},
-			{
-				selector: "typeLike",
-				format: ["PascalCase"],
-			},
-		],
+  extends: [
+    "eslint:recommended",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+    ecmaVersion: "latest",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: "module",
+  },
+  parser: "@typescript-eslint/parser",
+  env: {
+    es6: true,
+    es2020: true,
+    es2021: true,
+    commonjs: true,
+    node: true,
+    browser: true,
+    worker: true,
+    jest: true,
+    "shared-node-browser": true,
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+  },
+  plugins: ["@typescript-eslint", "prettier", "import"],
+  rules: {
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        types: ["boolean"],
+        format: ["PascalCase"],
+        prefix: ["is", "should", "has", "can", "did", "will"],
+      },
+      {
+        selector: "function",
+        format: ["PascalCase", "camelCase"],
+      },
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+    ],
 
-		"jest/expect-expect": "off",
+    "jest/expect-expect": "off",
 
-		"@rushstack/typedef-var": "off",
-	},
-	ignorePatterns: [
-		"**/*.json",
-		"node_modules",
-		"public",
-		"styles",
-		".next",
-		"coverage",
-		"dist",
-		"build",
-		".turbo",
-	],
+    "@rushstack/typedef-var": "off",
+  },
+  ignorePatterns: [
+    "**/*.json",
+    "node_modules",
+    "public",
+    "styles",
+    ".next",
+    "coverage",
+    "dist",
+    "build",
+    ".turbo",
+  ],
 };
